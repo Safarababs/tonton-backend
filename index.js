50fcf5cee4bf7de13c8fc9a28ce8c54785b922c1
SAFAR ABBAS <safarabbas73.sa@gmail.com>
2023-12-13 10:04:37 +0300

    safar

--- index.js
+++ index.js
@@ -4,24 +4,29 @@ const nodemailer = require("nodemailer");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 // Create a Nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: "hotmail", // Use your email service (e.g., 'hotmail' or 'outlook')
+  service: "gmail", // Use your email service (e.g., 'hotmail' or 'outlook')
   auth: {
-    user: "masoomasafar_2010@hotmail.com", // Your email
+    user: "safarabbas74.sa@gmail.com", // Your email
     pass: "sn5125a1", // Your password
   }, // Add this line for secure connection (if required)
+  tls: {
+    ciphers: 'SSLv3',
+    rejectUnauthorized: false,
+  },
 });
 
-app.use(express.json()); // Parse JSON bodies
+ // Parse JSON bodies
 
 app.post("/send-email", (req, res) => {
   const { name, email, number, message } = req.body;
 
   // Email data
   const mailOptions = {
-    from: "masoomasafar_2010@hotmail.com",
+    from: "safarabbas74.sa@gmail.com",
     to: "safarabbas_2010@hotmail.com",
     subject: "Business Email!",
     html: `<p><strong>Hi my name is ${name}</strong></p><p><strong>${message}</strong></p><p><strong>My number is ${number}</strong></p><p><strong>My email: ${email}</strong></p>`,
